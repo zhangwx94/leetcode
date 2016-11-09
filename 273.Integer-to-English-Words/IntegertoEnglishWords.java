f2598864edcdde4b60e21f45c48a0e420e8bfbab
zhangwx94 <zhangwx94@gmail.com>
2016-11-08 22:51:46 -0800

    Update IntegertoEnglishWords.java

--- 273.Integer-to-English-Words/IntegertoEnglishWords.java
+++ 273.Integer-to-English-Words/IntegertoEnglishWords.java
@@ -1,6 +1,8 @@
 public class Solution {
     int[] divisor = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20, 30, 40, 50, 60, 70, 80, 90};
-    String[] divisor_w = {"Zero","One ", "Two ", "Three ", "Four ", "Five ", "Six ", "Seven ", "Eight ", "Nine ", "Ten ","Eleven ", "Twelve ", "Thirteen ", "Fourteen ", "Fifteen ", "Sixteen ", "Seventeen ","Eighteen ", "Nineteen ","Twenty ", "Thirty ", "Forty ", "Fifty ", "Sixty ", "Seventy ", "Eighty ", "Ninety "};
+    String[] divisor_w = {"Zero","One ", "Two ", "Three ", "Four ", "Five ", "Six ", "Seven ", "Eight ", "Nine ", "Ten ",
+        "Eleven ", "Twelve ", "Thirteen ", "Fourteen ", "Fifteen ", "Sixteen ", "Seventeen ","Eighteen ", "Nineteen ",
+        "Twenty ", "Thirty ", "Forty ", "Fifty ", "Sixty ", "Seventy ", "Eighty ", "Ninety "};
     public String numberToWords(int num) {
         if(num == 0) return "Zero";
         String rlt = helper(num);
